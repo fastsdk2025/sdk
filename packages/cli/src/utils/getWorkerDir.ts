e8f2fa65c988d13c0eae00d1fe96cc20309cec24
tuiu <13719283454@163.com>
2025-10-21 12:23:52 +0800

    refactor(utils): add debug log for worker directory lookup

--- packages/cli/src/utils/getWorkerDir.ts
+++ packages/cli/src/utils/getWorkerDir.ts
@@ -4,6 +4,7 @@ import { findParent } from "./findParent";
 
 export function getWorkerDir(base: string, path: string) {
   const basePath = findParent(base, (dir) => {
+    console.log(join(dir, path));
     return existsSync(join(dir, path));
   });
 
