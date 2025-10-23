5735dc6bc5871483761ef3732bc3bef422ad1bfd
tuiu <13719283454@163.com>
2025-10-23 18:43:32 +0800

    feat(utils): add isCommandAvailable utility function to check command availability

---
+++ packages/cli/src/utils/isCommandAvailable.ts
@@ -0,0 +1,22 @@
+import { spawnSync } from "node:child_process";
+
+function isCommandAvailable(command: string): boolean {
+  let cmdName: string;
+  let args: string[];
+
+  if (process.platform === "win32") {
+    cmdName = "where";
+    args = [command];
+  } else {
+    cmdName = "which";
+    args = [command];
+  }
+
+  const cmd = spawnSync(cmdName, args, { encoding: "utf-8" });
+
+  if (!cmd.stdout || cmd.stdout.toString().trim() === "") {
+    return false;
+  }
+
+  return true;
+}
