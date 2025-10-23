b4d6c2e42dbe1af8f02780e27a59b207ed43ac31
tuiu <13719283454@163.com>
2025-10-24 02:26:36 +0800

    feat(core): add logger service registration in kernel boot
    Register LoggerService during kernel boot to enable logging functionality

--- packages/cli/src/core/Kernel.ts
+++ packages/cli/src/core/Kernel.ts
@@ -1,5 +1,6 @@
 import { Command } from "commander";
 import ServiceManager from "./ServiceManager";
+import LoggerService from "./services/logger/LoggerService";
 
 export default class Kernel extends Command {
   public readonly serviceManager!: ServiceManager;
@@ -8,5 +9,9 @@ export default class Kernel extends Command {
     super()
 
     this.serviceManager = new ServiceManager(this)
+  }
+
+  public boot() {
+    this.serviceManager.register("logger", LoggerService)
   }
 }
