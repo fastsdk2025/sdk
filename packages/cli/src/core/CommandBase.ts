63dba723b354b38e9c09c768ae2f5fa9a1881da8
tuiu <13719283454@163.com>
2025-10-29 01:23:04 +0800

    feat(core): add service access methods and lifecycle hooks to CommandBase
    Add getService and requireService methods for accessing services from CommandBase
    Introduce optional onDisable and onError lifecycle hooks

--- packages/cli/src/core/CommandBase.ts
+++ packages/cli/src/core/CommandBase.ts
@@ -1,5 +1,6 @@
 import { Command } from "commander";
 import Kernel from "./Kernel";
+import { ServiceInstance, ServiceName } from "./types";
 
 export default abstract class CommandBase {
   public readonly program: Command = new Command();
@@ -9,5 +10,16 @@ export default abstract class CommandBase {
     this.onEnable()
   }
 
+  protected getService<K extends ServiceName>(name: K): ServiceInstance<K> | undefined {
+    return this.kernel.serviceManager.get(name)
+  }
+
+  protected requireService<K extends ServiceName>(name: K): ServiceInstance<K> {
+    return this.kernel.serviceManager.require(name)
+  }
+
   abstract onEnable(): void;
+
+  public onDisable?(): void;
+  public onError?(error: Error): void;
 }
