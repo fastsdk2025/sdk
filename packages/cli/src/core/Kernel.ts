8537778191407f6891d8edeabc52f0e75de36cda
tuiu <13719283454@163.com>
2025-10-29 18:28:34 +0800

    style: fix missing semicolons in Kernel class

--- packages/cli/src/core/Kernel.ts
+++ packages/cli/src/core/Kernel.ts
@@ -7,19 +7,19 @@ export default class Kernel extends Command {
   public readonly serviceManager!: ServiceManager;
 
   constructor() {
-    super()
+    super();
 
-    this.serviceManager = new ServiceManager(this)
+    this.serviceManager = new ServiceManager(this);
   }
 
   public async boot() {
-    this.serviceManager.defineMultiple(serviceDefinitions)
+    this.serviceManager.defineMultiple(serviceDefinitions);
 
-    await this.serviceManager.initAll()
+    await this.serviceManager.initAll();
   }
 
   public registerCommand(commandClass: CommandConstructor) {
-    const command = new commandClass(this)
-    this.addCommand(command.program)
+    const command = new commandClass(this);
+    this.addCommand(command.program);
   }
 }
